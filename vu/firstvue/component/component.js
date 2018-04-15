c497f148a1f647b1f0c8f634a606598af726f405
RobbieLan <903133063@qq.com>
2018-04-15 10:26:43 +0800

    jj

--- vu/firstvue/component/component.js
+++ vu/firstvue/component/component.js
@@ -6,7 +6,7 @@ Vue.component('my-component', {
 
 // 创建根实例
 new Vue({
-  el: '#example'
+  el: '#app1'
 }) */
 /////////////////////////////////////////////////////////////////
 
@@ -16,9 +16,46 @@ var Child = {
 }
 
 new Vue({
-  el: '#example',
+  el: '#app1',
   components: {
     'my-component': Child
   }
 })
 //////////////////////////////////////////////////////////////////
+
+var myrow = {
+  template: '<tr>A custom component!</tr>'
+}
+
+new Vue({
+  el: '#app2',
+  components: {
+    'my-row': myrow
+  }
+})
+
+
+var data = { counter: 0 }
+Vue.component('simple-counter', {
+  template: '<button v-on:click="counter += 1">{{ counter }}</button>',
+  data: function () {
+    //return data  // 技术上 data 的确是一个函数了，因此 Vue 不会警告，但是我们却给每个组件实例返回了同一个对象的引用
+	return {       //为每个组件返回全新的数据对象来修复这个问题(三个按钮点击数同时增加的问题)
+		counter: 0
+	}
+  }
+})
+new Vue({
+  el: '#app3'
+})
+
+Vue.component('child', {
+  // 声明 props
+  props: ['myMessage'],
+  // 就像 data 一样，prop 也可以在模板中使用
+  // 同样也可以在 vm 实例中通过 this.message 来使用
+  template: '<span>{{ myMessage }}</span>'
+})
+new Vue({
+  el: '#app4'
+})
